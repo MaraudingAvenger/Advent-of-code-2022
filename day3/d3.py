985d4a48a6ccad9184e7df0751c54be935a843f0
Luke <lechambers@gmail.com>
2022-12-07 10:43:46 -0500

    Finished Day 3

---
+++ day3/d3.py
@@ -0,0 +1,31 @@
+import string
+
+with open('input.txt') as f:
+    lines = f.read().split()
+
+def score(letter:str) -> int:
+    if letter in string.ascii_lowercase:
+        return ord(letter) - 96
+    elif letter in string.ascii_uppercase:
+        return ord(letter) - 38
+    return 0
+
+def cut_line(line:str) -> tuple[str,str]:
+    return 
+
+total = 0
+for line in lines:
+    left, right = line[:len(line)//2], line[len(line)//2:]
+    total += sum(map(score, {l for l in left if l in right}))
+
+print(total)
+
+
+badge_total = 0
+for i in range(0, len(lines), 3):
+    group = lines[i:i+3]
+    for letter in set(group[0]):
+        if all(letter in line for line in group[1:]):
+            badge_total += score(letter)
+
+print(badge_total)
