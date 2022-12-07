229dd74d7a41331621b00a7aa1603f840f700d1c
Luke <lechambers@gmail.com>
2022-12-07 11:29:55 -0500

    Completed day 4, updated gitignore

---
+++ day4/d4.py
@@ -0,0 +1,21 @@
+nums = [[[int(x) for x in pair.split('-')] for pair in line.split(',')] for line in open('input.txt').read().split('\n')]
+
+contained = 0
+for n in nums:
+    a, b = n
+    if (a[0] <= b[0] and a[1] >= b[1]):
+        contained += 1
+    elif (a[0] >= b[0] and a[1] <= b[1]):
+        contained += 1
+
+print(contained)
+
+overlap = 0
+for n in nums:
+    a, b = n
+    a = set(range(a[0],a[1]+1))
+    b = set(range(b[0],b[1]+1))
+    if a.intersection(b):
+        overlap += 1
+
+print(overlap)
